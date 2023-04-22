5e79dbb679c2db4d4949db31268ce89a68d2ee67
Enrique Saracho Felix <esarachof97@gmail.com>
2023-04-22 00:52:36 -0700

    App.css modified
    worked on responsiveness

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <div className="component">
+    <div className="component container2">
       <h2 className="title">Projects</h2>
       <div className="projects">
         <Project
@@ -8,6 +8,36 @@ export default function Projects() {
           title="Checkers Pizza"
           desc="For my Web Development I course project, I created a website for a fictional pizza place called ‘Checkers’. It has four pages, a home page with cool images and posts, a menu page with all imaginary items available, a cart page and an account page. I got to use my skills learned throughout the course, including HTML tables, forms, lists, CSS styles, layouts, responsiveness, and JavaScript functionality."
           skills={["HTML", "CSS", "JavaScript"]}
+        />
+        <Project
+          img={require("../images/comming-soon.jpg")}
+          title="Game List"
+          desc="MERN Stack application coming soon!"
+          skills={["mongodb", "Express.js", "React", "Node.js"]}
+        />
+        <Project
+          img={require("../images/comming-soon.jpg")}
+          title="Game List"
+          desc="MERN Stack application coming soon!"
+          skills={["mongodb", "Express.js", "React", "Node.js"]}
+        />
+        <Project
+          img={require("../images/comming-soon.jpg")}
+          title="Game List"
+          desc="MERN Stack application coming soon!"
+          skills={["mongodb", "Express.js", "React", "Node.js"]}
+        />
+        <Project
+          img={require("../images/comming-soon.jpg")}
+          title="Game List"
+          desc="MERN Stack application coming soon!"
+          skills={["mongodb", "Express.js", "React", "Node.js"]}
+        />
+        <Project
+          img={require("../images/comming-soon.jpg")}
+          title="Game List"
+          desc="MERN Stack application coming soon!"
+          skills={["mongodb", "Express.js", "React", "Node.js"]}
         />
         <Project
           img={require("../images/comming-soon.jpg")}
