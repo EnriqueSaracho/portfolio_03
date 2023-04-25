8cfb4daf30e0f2194f03fd8da84c8973552eda2f
Enrique Saracho Felix <esarachof97@gmail.com>
2023-04-24 18:32:45 -0700

    checkers.js modified
    added correct url

--- src/pages/checkers.js
+++ src/pages/checkers.js
@@ -9,7 +9,7 @@ export default function Checkers() {
         img={require("../images/checkers-big.jpg")}
         title="Checkers Pizza"
         desc="For my Web Development I course project, I created a website for a fictional pizza place called ‘Checkers’. It has four pages, a home page with cool images and posts, a menu page with all imaginary items available, a cart page and an account page. I got to use the skills learned throughout the course, including HTML tables, forms, lists, CSS styles, layouts, responsiveness, and JavaScript functionality."
-        btn1={"#"}
+        btn1={"https://enriquesaracho.github.io/checkers/index.html"}
         btn1Text={"See Web page"}
         btn2={"https://github.com/EnriqueSaracho/checkers"}
         btn2Text={"See Code"}
