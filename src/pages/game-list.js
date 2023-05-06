3cf06f7e6eb705a68af10c21064d054dc552a825
Enrique Saracho Felix <esarachof97@gmail.com>
2023-05-06 11:09:35 -0700

    game-list modified
    check spelling

--- src/pages/game-list.js
+++ src/pages/game-list.js
@@ -8,8 +8,8 @@ export default function GameList() {
       <Project
         img={require("../images/game-list-big.jpg")}
         title="Game List"
-        desc="This MERN Stack application is a list of games that I've played and have yet to play. It allows me to input and display some information about each and also rate them based on my subjective opinion. Working on this project has allowed me to learn a few things like importing react components, React Router, Environmental Variables, HTTP Requests, useNavigate(), useState(), React Icons, Database Schemas, and more. (Not complete yet)"
-        btn1={"https://github.com/EnriqueSaracho/portfolio_03"}
+        desc="(Not complete yet) This MERN Stack application is a list of games that I have played and have yet to play. It allows me to input and display some information about each and rate them based on my subjective opinion. Working on this project has allowed me to learn a few things like importing react components, React Router, Environmental Variables, HTTP Requests, useNavigate(), useState(), React Icons, Database Schemas, and more."
+        btn1={"https://github.com/EnriqueSaracho/game-list"}
         btn1Text={"See Code"}
         skills={["mongoDB", "Express.js", "React", "Node.js"]}
       />
